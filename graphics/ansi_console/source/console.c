7307a61e8c0f8360cf749f1f7969054f7aa1732d
Dave Murphy <wntrmute@users.sourceforge.net>
2005-09-12 22:51:08 +0000

    added ansi test demo

---
+++ graphics/ansi_console/source/console.c
@@ -0,0 +1,66 @@
+
+#include <gba_console.h>
+#include <gba_video.h>
+#include <gba_interrupt.h>
+
+#include <stdio.h>
+
+//---------------------------------------------------------------------------------
+// Program entry point
+//---------------------------------------------------------------------------------
+int main(void) {
+//---------------------------------------------------------------------------------
+
+	// the vblank interrupt must be enabled for VBlankIntrWait() to work
+	// since the default dispatcher handles the bios flags no vblank handler
+	// is required
+	InitInterrupt();
+	EnableInterrupt(Int_Vblank);
+	
+	// initialise the console
+	// setting NULL & 0 for the font address & size uses the default font
+	// The font should be a complete 1bit 8x8 ASCII font
+	consoleInit(	0,		/* charbase */
+					4,		/* mapbase */
+					0,		/* background number */
+					NULL,	/* font */
+					0, 		/* font size */
+					15		/* 16 color palette */);
+
+	// set the screen colors, color 0 is the background color
+	// the foreground color is index 1 of the selected 16 color palette 
+	BG_COLORS[0]=RGB8(58,110,165);
+	BG_COLORS[241]=RGB5(31,31,31);
+
+	SetMode(MODE_0 | BG0_ON);
+	
+	// ansi escape sequence to clear screen and home cursor
+	// /x1b[line;columnH
+	iprintf("\x1b[2J");
+
+	// ansi escape sequence to set print co-ordinates
+	// /x1b[line;columnH
+	iprintf("\x1b[10;10HHello World!");
+
+	// ansi escape sequence to move cursor up
+	// /x1b[linesA
+	iprintf("\x1b[10ALine 0");
+
+	// ansi escape sequence to move cursor left
+	// /x1b[columnsD
+	iprintf("\x1b[28DColumn 0");
+
+	// ansi escape sequence to move cursor down
+	// /x1b[linesB
+	iprintf("\x1b[19BLine 19");
+
+	// ansi escape sequence to move cursor right
+	// /x1b[columnsC
+	iprintf("\x1b[5CColumn 20");
+
+	while (1) {
+		VBlankIntrWait();
+	}
+}
+
+
