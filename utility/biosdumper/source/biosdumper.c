84f370c6caa0b30cdfa67222308d8af8187f67d4
Dave Murphy <wntrmute@users.sourceforge.net>
2007-02-08 21:30:22 +0000

    add bios dumper code

---
+++ utility/biosdumper/source/biosdumper.c
@@ -0,0 +1,118 @@
+/*---------------------------------------------------------------------------------
+
+	Some emulators need the bios from your gba in order to use SWI calls
+	
+	This little piece of code will read the bios and save it to your
+	SD/CF card device using the magic of libfat.
+	
+	Some cards are supported be default, the binary will need patched with
+	the appropriate DLDI file for newer cards.
+
+---------------------------------------------------------------------------------*/
+
+
+#include <gba.h>
+#include <fat.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//---------------------------------------------------------------------------------
+void waitForever() {
+//---------------------------------------------------------------------------------
+	while (1)
+		VBlankIntrWait();
+}
+
+//---------------------------------------------------------------------------------
+// Program entry point
+//---------------------------------------------------------------------------------
+int main(void) {
+//---------------------------------------------------------------------------------
+
+
+	// the vblank interrupt must be enabled for VBlankIntrWait() to work
+	// since the default dispatcher handles the bios flags no vblank handler
+	// is required
+	irqInit();
+	irqEnable(IRQ_VBLANK);
+
+	// initialise the console
+	// setting NULL & 0 for the font address & size uses the default font
+	// The font should be a complete 1bit 8x8 ASCII font
+	consoleInit(	0,		// charbase
+					4,		// mapbase
+					0,		// background number
+					NULL,	// font
+					0, 		// font size
+					15		// 16 color palette
+				);
+
+	// RGB8 generates a 16 bit palette entry from 3 8bit components
+	BG_COLORS[0]=RGB8(58,110,165);
+
+	// RGB5 generates a 16 bit palette entry from 3 5bit components
+	BG_COLORS[241]=RGB5(31,31,31);
+
+	SetMode(MODE_0 | BG0_ON);
+
+	iprintf("GBA Bios Dumper\n\n");
+
+	if (fatInitDefault()) {
+		iprintf("FAT system initialised\n");
+	} else {
+		iprintf("FAT system failed!\n");
+		waitForever();
+	}
+	
+	u32 *bios = (u32 *)malloc(0x4000);
+	
+	if ( bios ) {
+		iprintf("Memory allocated\n");
+	} else {
+		iprintf("Memory allocation failure!\n");
+		waitForever();
+	}
+
+	int i;
+	
+	iprintf("dumping ");
+
+	for (i=0; i<0x4000; i+=4)
+	{
+		// The MidiKey2Freq bios call allows us to read from bios
+		// the lower bits are inaccurate, so just get it four times :)
+		u32 a = MidiKey2Freq((WaveData *)(i-4), 180-12, 0) * 2;
+		u32 b = MidiKey2Freq((WaveData *)(i-3), 180-12, 0) * 2;
+		u32 c = MidiKey2Freq((WaveData *)(i-2), 180-12, 0) * 2;
+		u32 d = MidiKey2Freq((WaveData *)(i-1), 180-12, 0) * 2;
+
+		// rebuild a 32bit word from the 4 words we read
+		u32 abcd =	( a & 0xff000000 ) |
+					( d & 0xff000000 ) >> 8 |
+					( c & 0xff000000 ) >> 16 |
+					( b & 0xff000000 ) >> 24;
+		bios[i/4] = abcd;
+
+		//print a dot every 256 bytes
+		if ( (i & 0xff) == 0 ) iprintf("."); 
+
+	}
+	
+	iprintf("\nBios dumped, saving file\n");
+	
+	FILE *biosFile = fopen("biosgba.rom","wb");
+	if (biosFile ) {
+		fwrite(bios,16384,1,biosFile);
+		fclose(biosFile);
+		iprintf("bios saved!");
+	} else {
+		iprintf("file creation failed!");
+	}
+
+	waitForever();
+
+	return 0;
+}
+
+
